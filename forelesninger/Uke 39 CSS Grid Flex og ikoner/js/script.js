16f500a46fdc6ce8d076e7acf55f5da68a1449e7
Veexly <alvin@stude.no>
2026-09-25 11:09:57 +0200

    wb forelesning 25. september 2026

---
+++ forelesninger/Uke 39 CSS Grid Flex og ikoner/js/script.js
@@ -0,0 +1,20 @@
+// Henter HTML-elementet med id="ham"
+const ham = document.getElementById("ham")
+
+const closeHam = document.getElementById('closeHam')
+
+const menu = document.getElementById('menu')
+
+
+ham.addEventListener('click', ()=>{
+    menu.style.display = 'flex';
+
+
+    console.log("Knappen er trykket på")
+})
+
+closeHam.addEventListener('click', ()=>{
+    menu.style.display = 'none'
+
+    console.log('Menyen er lukket')
+})
